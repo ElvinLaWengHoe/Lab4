2bafff5e9a73058af64e771a7fe2bcd74007078c
Elvin <ELVINLA.22@ichat.sp.edu.sg>
2023-11-07 23:35:57 +0800

    Exercise 2

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -1,16 +1,23 @@
 from hal import hal_input_switch as switch
 from hal import hal_led as led
-from time import sleep
+from time import sleep, time
 
 def main():
     switch.init()
     led.init()
-    while():
-        if switch.read_slide_switch() == 0:
+    while(1):
+        if switch.read_slide_switch():
             led.set_output(0,1)
-            sleep(0.2)
+            sleep(0.1)
             led.set_output(0,0)
-            sleep(0.2)
+            sleep(0.1)
+        else:
+            time_finish = time() + 5
+            while time() < time_finish:
+             led.set_output(0,1)
+             sleep(0.05)
+             led.set_output(0,0)
+             sleep(0.05)
 
 if __name__ == "__main__":
     main()
